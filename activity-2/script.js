5e0eba222a250f40d5c5516cf53c898ca83b7512
ShuaibKangan <200083238@student.kangan.edu.au>
2026-08-25 12:18:58 +1000

    Activity 2 completed!

--- activity-2/script.js
+++ activity-2/script.js
@@ -28,3 +28,36 @@
 
 // TODO Step 4: Fetch all recipes
 // fetch('https://dummyjson.com/recipes').then(onResponseReady);
+
+const recipeList = document.getElementById('recipeList');
+const status = document.getElementById('status');
+
+function onDataReady(data) {
+    const recipes = data.recipes;
+
+    status.innerText = 'Found ' + recipes.length + ' recipes';
+
+    let html = '';
+    for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
+        html = html + '<div class="card">';
+        hmtl = html + '<img src="' + recipe.image + '" alt="' + recipe.name + '">';
+        html = html + '<div class="card-body">';
+        html = html + '<h3>' + recipe.name + '</h3>';
+        html = html + '<p class="meta">' + recipe.cuisine + ' . ' + recipe.difficulty + '</p>';
+        html = html + '<p class="rating">★ ' + recipe.rating + '<p/>';
+        html = html + '<p class="time">prep: ' + recipe.prepTimeMinutes + ' mins</p>';
+        html = html + '</div>';
+        html = html + '</div>';
+    }
+
+    recipeList.innerHTML = html;
+}
+
+function onResponseReady(response) {
+    response.json().then(onDataReady);
+}
+
+fetch('https://dummyjson.com/recipes').then(onResponseReady);   
+
+
